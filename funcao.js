56445bc0890093ab93614a387850336ca1e5ced4
KAikiGma <Kaikigamaalborgueti@gmail.com>
2022-05-16 20:36:13 -0300

    Cinema

--- funcao.js
+++ funcao.js
@@ -1,8 +1,8 @@
-var imagens =["imagens/batman.jpg", "imagens/spiderman.jpg", "imagens/doutor-estranho.webp", "imagens/Vingadores-Ultimato.jpg", "imagens/velosesefuriosos.jpg"]
+var imagens =["imagens/batman.jpg", "imagens/spiderman.jpg", "imagens/doutor-estranho.webp", "imagens/Vingadores-Ultimato.jpg", "imagens/velosesefuriosos.jpg", "imagens/jujutsu-kaisen-0-plano-critico.jpg"]
 var imagemAtual = 0;
 
 function trocarImagem() {
-    imagemAtual = (imagemAtual + 1) % 5;
+    imagemAtual = (imagemAtual + 1) % 6;
     document.querySelector('.gif img').src = imagens [imagemAtual];
 }
 
